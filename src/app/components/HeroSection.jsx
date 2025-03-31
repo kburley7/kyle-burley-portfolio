79dc503bc1b13151736fc7c37834883c397e56e6
kburley7 <kburley7@uwo.ca>
2025-03-31 16:35:11 -0400

    finished hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
 
@@ -8,18 +10,34 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 lg:grid-cols-12">
                 <div className="col-span-7 place-self-center">
                     <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-                        Hello, I'm Kyle
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-50 to-purple-500">
+                            Hello, I'm{" "}
+                            <br/>
+                            <TypeAnimation
+                                sequence={[
+                                    "Kyle",
+                                    1000,
+                                    "Web Developer",
+                                    1000,
+                                    "Software Engineer",
+                                    1000,
+                                ]}
+                                wrapper='span'
+                                speed={50}
+                                repeat={Infinity}
+                            />
+                        </span>
                     </h1>
                     <p className='text-[#ADB7BE] text-lg lg:text-xl mb-6'>
-                        Write Some Stuff about me here
+                        Write Some Cool Stuff about me here
                     </p>
                     <div className='py-2 '>
                         <a href="mailto:burleykyle10@gmail.com">
-                            <button className='px-6 py-3 rounded-full mr-4 bg-[#4F2683] hover:bg-purple-400 text-white'>Email Me</button>
+                            <button className='px-6 py-3 rounded-full mr-4 bg-[#4F2683] hover:bg-purple-400 text-white' >Email Me</button>
                         </a>
                         <a href="\files\Kyle_Burley_Software_Developer.pdf" download>
                             <button className='px-6 py-3 rounded-full  bg-[#4F2683] hover:bg-purple-400 text-white'>Download Resume</button>
-                        </a>                        
+                        </a>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center'>
